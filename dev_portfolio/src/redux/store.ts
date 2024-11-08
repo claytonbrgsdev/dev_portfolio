4c8d9d1f728ca9f8b9b73e1521cfd148a4981780
claytonbrgs <142549801+claytonbrgs@users.noreply.github.com>
2024-11-08 08:36:17 -0300

    redux store

--- dev_portfolio/src/redux/store.ts
+++ dev_portfolio/src/redux/store.ts
@@ -1,52 +1,36 @@
-// // src/redux/store.ts
+// src/redux/store.ts
 
-// import { configureStore, combineReducers } from '@reduxjs/toolkit';
-// import storage from 'redux-persist/lib/storage';
-// import { persistStore, persistReducer } from 'redux-persist';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer } from 'redux-persist';
 
-// // Rename imports to clearly indicate they are reducers
-// import authReducer from './slices/authSlice';
-// import productsReducer from './slices/productsSlice';
-// import cartReducer from './slices/cartSlice';
-// import artistsReducer from './slices/artistsSlice';
-// import releasesReducer from './slices/releasesSlice';
-// import mediaReducer from './slices/mediaSlice';
-// import radioshowsReducer from './slices/radioshowsSlice';
-// import orderReducer from './slices/orderSlice';
-// import currencyReducer from '@redux/slices/currencySlice';
+// Rename imports to clearly indicate they are reducers
+import authReducer from './slices/authSlice';
 
-// const rootReducer = combineReducers({
-//   auth: authReducer,
-//   products: productsReducer,
-//   cart: cartReducer,
-//   artists: artistsReducer,
-//   releases: releasesReducer,
-//   media: mediaReducer,
-//   radioshows: radioshowsReducer,
-//   orders: orderReducer,
-//   currency: currencyReducer,
-// });
+const rootReducer = combineReducers({
+  auth: authReducer,
+});
 
-// const persistConfig = {
-//   key: 'root',
-//   storage,
-//   whitelist: ['auth', 'cart'], // Only persist auth and cart slices
-// };
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['auth', 'cart'], // Only persist auth and cart slices
+};
 
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-// export const store = configureStore({
-//   reducer: persistedReducer,
-//   middleware: (getDefaultMiddleware) =>
-//     getDefaultMiddleware({
-//       serializableCheck: {
-//         ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
-//       },
-//     }),
-// });
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+      },
+    }),
+});
 
-// export const persistor = persistStore(store);
+export const persistor = persistStore(store);
 
-// // **RootState and AppDispatch Types**
-// export type RootState = ReturnType<typeof rootReducer>;
-// export type AppDispatch = typeof store.dispatch;
+// **RootState and AppDispatch Types**
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppDispatch = typeof store.dispatch;
