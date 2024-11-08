d7255acc0a7f71f30df8a61e5fdb582ad1cf2e01
claytonbrgs <142549801+claytonbrgs@users.noreply.github.com>
2024-11-08 08:55:30 -0300

    main updated

--- dev_portfolio/src/main.tsx
+++ dev_portfolio/src/main.tsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { RouterProvider } from 'react-router-dom';
+import { UserProvider } from './context/user/UserProvider';
+import React from 'react';
+import '@styles/index.css';
+import { Provider } from 'react-redux';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { createRoot } from 'react-dom/client';
+import AuthObserver from './utils/authObserver/AuthObserver';
+import '@styles/OverrideFonts.css';
+import { router } from './routes/Routes';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <UserProvider>
+            <AuthObserver />
+            <RouterProvider router={router} />
+          </UserProvider>
+        </PersistGate>
+      </Provider>
+    </React.StrictMode>
+  );
+}
+
