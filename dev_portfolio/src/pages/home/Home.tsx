5be63de7d39ee073ddbc4586db94fdd265e9d5a8
claytonbrgs <142549801+claytonbrgs@users.noreply.github.com>
2024-11-08 09:12:04 -0300

    Home component and css module added

---
+++ dev_portfolio/src/pages/home/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Home.module.css'; 
+
+const Home: React.FC = () => {
+
+    return (
+        <div className={styles.home}>
+            <div className={styles.homeSection}>
+
+            </div>
+        </div>
+    );
+};
+
+export default Home;
