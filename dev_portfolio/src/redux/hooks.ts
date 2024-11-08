d0bf2e3f21ca3c04aa257f2a618b4809ac538d82
claytonbrgs <142549801+claytonbrgs@users.noreply.github.com>
2024-11-08 08:37:29 -0300

    redux hooks

--- dev_portfolio/src/redux/hooks.ts
+++ dev_portfolio/src/redux/hooks.ts
@@ -1,8 +1,8 @@
-// import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
-// import { RootState } from './store';
-// import { store } from './store';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+import { RootState } from './store';
+import { store } from './store';
 
-// type AppDispatch = typeof store.dispatch;
+type AppDispatch = typeof store.dispatch;
 
-// export const useAppDispatch = () => useDispatch<AppDispatch>();
-// export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
