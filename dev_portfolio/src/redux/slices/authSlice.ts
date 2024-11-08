992c544c5bd5402607e6e31054baadd86efabd59
claytonbrgs <142549801+claytonbrgs@users.noreply.github.com>
2024-11-08 08:38:27 -0300

    auth slice

--- dev_portfolio/src/redux/slices/authSlice.ts
+++ dev_portfolio/src/redux/slices/authSlice.ts
@@ -1,281 +1,281 @@
-// // src/redux/slices/authSlice.ts
+// src/redux/slices/authSlice.ts
 
-// import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
-// import {
-//   signInWithEmailAndPassword,
-//   signOut as firebaseSignOut,
-// } from 'firebase/auth';
-// import { User } from 'docTypes/user';
-// import { RootState } from '@redux/store';
-// import { doc, getDoc, setDoc } from 'firebase/firestore';
-// import { db, auth } from '@services/firebase'; // Import auth from firebase.ts
-// import { setCurrency } from './currencySlice'; // Import setCurrency action
+import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
+import {
+  signInWithEmailAndPassword,
+  signOut as firebaseSignOut,
+} from 'firebase/auth';
+import { User } from 'docTypes/user';
+import { RootState } from '@redux/store';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
+import { db, auth } from '@services/firebase'; // Import auth from firebase.ts
 
-// // Define the structure of the authentication state
-// type AuthState = {
-//   authenticated: boolean;
-//   currentUser: User | null;
-//   loading: boolean;
-//   error: string | null;
-// };
 
-// // Initialize the authentication state
-// const initialState: AuthState = {
-//   authenticated: false,
-//   currentUser: null,
-//   loading: false,
-//   error: null,
-// };
+// Define the structure of the authentication state
+type AuthState = {
+  authenticated: boolean;
+  currentUser: User | null;
+  loading: boolean;
+  error: string | null;
+};
 
-// // -------------------- Thunks --------------------
+// Initialize the authentication state
+const initialState: AuthState = {
+  authenticated: false,
+  currentUser: null,
+  loading: false,
+  error: null,
+};
 
-// // Sign In Thunk
-// export const signIn = createAsyncThunk<
-//   User,
-//   { email: string; password: string },
-//   { state: RootState; rejectValue: string }
-// >(
-//   'auth/signIn',
-//   async ({ email, password }, { rejectWithValue, dispatch }) => {
-//     try {
-//       const userCredential = await signInWithEmailAndPassword(
-//         auth,
-//         email,
-//         password
-//       );
+// -------------------- Thunks --------------------
 
-//       // Ensure the user's email is verified
-//       if (!userCredential.user.emailVerified) {
-//         throw new Error('Please verify your email before signing in.');
-//       }
+// Sign In Thunk
+export const signIn = createAsyncThunk<
+  User,
+  { email: string; password: string },
+  { state: RootState; rejectValue: string }
+>(
+  'auth/signIn',
+  async ({ email, password }, { rejectWithValue, dispatch }) => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
 
-//       const uid = userCredential.user.uid;
-//       const userDocRef = doc(db, 'users', uid);
-//       const userSnapshot = await getDoc(userDocRef);
+      // Ensure the user's email is verified
+      if (!userCredential.user.emailVerified) {
+        throw new Error('Please verify your email before signing in.');
+      }
 
-//       if (userSnapshot.exists()) {
-//         const userData = userSnapshot.data() as User;
-//         const user: User = { ...userData, uid };
+      const uid = userCredential.user.uid;
+      const userDocRef = doc(db, 'users', uid);
+      const userSnapshot = await getDoc(userDocRef);
 
-//         // Dispatch setCurrency based on user's country
-//         if (user.country?.toLowerCase() === 'brazil') {
-//           dispatch(setCurrency('BRL'));
-//         } else {
-//           dispatch(setCurrency('USD'));
-//         }
+      if (userSnapshot.exists()) {
+        const userData = userSnapshot.data() as User;
+        const user: User = { ...userData, uid };
 
-//         return user;
-//       } else {
-//         throw new Error('User details not found.');
-//       }
-//     } catch (error: unknown) {
-//       if (error instanceof Error) {
-//         if ('code' in error) {
-//           return rejectWithValue((error as { code: string }).code);
-//         } else {
-//           return rejectWithValue(error.message);
-//         }
-//       } else {
-//         return rejectWithValue('An unknown error occurred during sign in.');
-//       }
-//     }
-//   }
-// );
+        // Dispatch setCurrency based on user's country
+        if (user.country?.toLowerCase() === 'brazil') {
+          dispatch(setCurrency('BRL'));
+        } else {
+          dispatch(setCurrency('USD'));
+        }
 
-// // Fetch User Details Thunk
-// export const fetchUserDetails = createAsyncThunk<
-//   User,
-//   string,
-//   { state: RootState; rejectValue: string }
-// >(
-//   'auth/fetchUserDetails',
-//   async (uid, { rejectWithValue, dispatch }) => {
-//     try {
-//       const userDocRef = doc(db, 'users', uid);
-//       const userSnapshot = await getDoc(userDocRef);
+        return user;
+      } else {
+        throw new Error('User details not found.');
+      }
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        if ('code' in error) {
+          return rejectWithValue((error as { code: string }).code);
+        } else {
+          return rejectWithValue(error.message);
+        }
+      } else {
+        return rejectWithValue('An unknown error occurred during sign in.');
+      }
+    }
+  }
+);
 
-//       if (userSnapshot.exists()) {
-//         const userData = userSnapshot.data();
-//         if (userData) {
-//           const user: User = { ...(userData as User), uid };
+// Fetch User Details Thunk
+export const fetchUserDetails = createAsyncThunk<
+  User,
+  string,
+  { state: RootState; rejectValue: string }
+>(
+  'auth/fetchUserDetails',
+  async (uid, { rejectWithValue, dispatch }) => {
+    try {
+      const userDocRef = doc(db, 'users', uid);
+      const userSnapshot = await getDoc(userDocRef);
 
-//           // Dispatch setCurrency based on user's country
-//           if (user.country?.toLowerCase() === 'brazil') {
-//             dispatch(setCurrency('BRL'));
-//           } else {
-//             dispatch(setCurrency('USD'));
-//           }
+      if (userSnapshot.exists()) {
+        const userData = userSnapshot.data();
+        if (userData) {
+          const user: User = { ...(userData as User), uid };
 
-//           return user;
-//         } else {
-//           throw new Error('Invalid user data format.');
-//         }
-//       } else {
-//         throw new Error('User details not found.');
-//       }
-//     } catch (error: unknown) {
-//       if (error instanceof Error) {
-//         return rejectWithValue(error.message);
-//       } else {
-//         return rejectWithValue(
-//           'An unknown error occurred while fetching user details.'
-//         );
-//       }
-//     }
-//   }
-// );
+          // Dispatch setCurrency based on user's country
+          if (user.country?.toLowerCase() === 'brazil') {
+            dispatch(setCurrency('BRL'));
+          } else {
+            dispatch(setCurrency('USD'));
+          }
 
-// // Sign Out Thunk
-// export const signOutUser = createAsyncThunk<void, void, { state: RootState; rejectValue: string }>(
-//   'auth/signOut',
-//   async (_, { rejectWithValue }) => {
-//     try {
-//       await firebaseSignOut(auth);
-//     } catch (error: unknown) {
-//       if (error instanceof Error) {
-//         return rejectWithValue(error.message);
-//       }
-//       return rejectWithValue('An unknown error occurred during sign out.');
-//     }
-//   }
-// );
+          return user;
+        } else {
+          throw new Error('Invalid user data format.');
+        }
+      } else {
+        throw new Error('User details not found.');
+      }
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        return rejectWithValue(error.message);
+      } else {
+        return rejectWithValue(
+          'An unknown error occurred while fetching user details.'
+        );
+      }
+    }
+  }
+);
 
-// // Update User Details Thunk
-// export const updateUserDetails = createAsyncThunk<
-//   User,
-//   User,
-//   { state: RootState; rejectValue: string }
-// >(
-//   'user/updateUserDetails',
-//   async (userData, { rejectWithValue }) => {
-//     const user = auth.currentUser;
-//     if (user) {
-//       try {
-//         await setDoc(doc(db, 'users', user.uid), userData, { merge: true });
-//         return userData;
-//       } catch (error: unknown) {
-//         if (error instanceof Error) {
-//           return rejectWithValue(error.message);
-//         } else {
-//           return rejectWithValue('An unknown error occurred while updating user details.');
-//         }
-//       }
-//     } else {
-//       return rejectWithValue('No user found');
-//     }
-//   }
-// );
+// Sign Out Thunk
+export const signOutUser = createAsyncThunk<void, void, { state: RootState; rejectValue: string }>(
+  'auth/signOut',
+  async (_, { rejectWithValue }) => {
+    try {
+      await firebaseSignOut(auth);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        return rejectWithValue(error.message);
+      }
+      return rejectWithValue('An unknown error occurred during sign out.');
+    }
+  }
+);
 
-// // Selectors
-// export const selectIsAdmin = (state: RootState) => {
-//   return state.auth.currentUser?.isAdmin || false;
-// };
+// Update User Details Thunk
+export const updateUserDetails = createAsyncThunk<
+  User,
+  User,
+  { state: RootState; rejectValue: string }
+>(
+  'user/updateUserDetails',
+  async (userData, { rejectWithValue }) => {
+    const user = auth.currentUser;
+    if (user) {
+      try {
+        await setDoc(doc(db, 'users', user.uid), userData, { merge: true });
+        return userData;
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          return rejectWithValue(error.message);
+        } else {
+          return rejectWithValue('An unknown error occurred while updating user details.');
+        }
+      }
+    } else {
+      return rejectWithValue('No user found');
+    }
+  }
+);
 
-// // -------------------- Slice --------------------
+// Selectors
+export const selectIsAdmin = (state: RootState) => {
+  return state.auth.currentUser?.isAdmin || false;
+};
 
-// export const authSlice = createSlice({
-//   name: 'auth',
-//   initialState,
-//   reducers: {
-//     // Update user with partial data
-//     updateUser: (state, action: PayloadAction<Partial<User>>) => {
-//       if (state.currentUser && action.payload) {
-//         state.currentUser = {
-//           ...state.currentUser,
-//           ...action.payload,
-//         };
-//       }
-//     },
-//     // Authenticate user
-//     authenticateUser: (state) => {
-//       state.authenticated = true;
-//     },
-//     // Set user directly
-//     setUser: (state, action: PayloadAction<User>) => {
-//       state.currentUser = action.payload;
-//     },
-//     // Sign out user
-//     signOut: (state) => {
-//       state.authenticated = false;
-//       state.currentUser = null;
-//     },
-//   },
-//   extraReducers: (builder) => {
-//     builder
-//       // Handle signIn Thunk
-//       .addCase(signIn.pending, (state) => {
-//         state.loading = true;
-//         state.error = null;
-//       })
-//       .addCase(signIn.fulfilled, (state, action: PayloadAction<User>) => {
-//         state.authenticated = true;
-//         state.currentUser = action.payload;
-//         state.loading = false;
-//         state.error = null;
-//       })
-//       .addCase(signIn.rejected, (state, action) => {
-//         state.loading = false;
-//         state.authenticated = false;
-//         state.currentUser = null;
-//         state.error =
-//           typeof action.payload === 'string'
-//             ? action.payload
-//             : 'An unknown error occurred during sign in.';
-//       })
+// -------------------- Slice --------------------
 
-//       // Handle fetchUserDetails Thunk
-//       .addCase(fetchUserDetails.fulfilled, (state, action: PayloadAction<User>) => {
-//         state.currentUser = action.payload;
-//         state.authenticated = true;
-//         state.loading = false;
-//         state.error = null;
-//       })
-//       .addCase(fetchUserDetails.rejected, (state, action) => {
-//         state.error =
-//           typeof action.payload === 'string'
-//             ? action.payload
-//             : 'An unknown error occurred while fetching user details.';
-//       })
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    // Update user with partial data
+    updateUser: (state, action: PayloadAction<Partial<User>>) => {
+      if (state.currentUser && action.payload) {
+        state.currentUser = {
+          ...state.currentUser,
+          ...action.payload,
+        };
+      }
+    },
+    // Authenticate user
+    authenticateUser: (state) => {
+      state.authenticated = true;
+    },
+    // Set user directly
+    setUser: (state, action: PayloadAction<User>) => {
+      state.currentUser = action.payload;
+    },
+    // Sign out user
+    signOut: (state) => {
+      state.authenticated = false;
+      state.currentUser = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      // Handle signIn Thunk
+      .addCase(signIn.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(signIn.fulfilled, (state, action: PayloadAction<User>) => {
+        state.authenticated = true;
+        state.currentUser = action.payload;
+        state.loading = false;
+        state.error = null;
+      })
+      .addCase(signIn.rejected, (state, action) => {
+        state.loading = false;
+        state.authenticated = false;
+        state.currentUser = null;
+        state.error =
+          typeof action.payload === 'string'
+            ? action.payload
+            : 'An unknown error occurred during sign in.';
+      })
 
-//       // Handle signOutUser Thunk
-//       .addCase(signOutUser.fulfilled, (state) => {
-//         state.authenticated = false;
-//         state.currentUser = null;
-//         state.loading = false;
-//         state.error = null;
-//       })
-//       .addCase(signOutUser.rejected, (state, action) => {
-//         state.error =
-//           typeof action.payload === 'string'
-//             ? action.payload
-//             : 'An unknown error occurred during sign out.';
-//       })
+      // Handle fetchUserDetails Thunk
+      .addCase(fetchUserDetails.fulfilled, (state, action: PayloadAction<User>) => {
+        state.currentUser = action.payload;
+        state.authenticated = true;
+        state.loading = false;
+        state.error = null;
+      })
+      .addCase(fetchUserDetails.rejected, (state, action) => {
+        state.error =
+          typeof action.payload === 'string'
+            ? action.payload
+            : 'An unknown error occurred while fetching user details.';
+      })
 
-//       // Handle updateUserDetails Thunk
-//       .addCase(updateUserDetails.fulfilled, (state, action: PayloadAction<User>) => {
-//         state.currentUser = action.payload;
-//         state.loading = false;
-//         state.error = null;
-//       })
-//       .addCase(updateUserDetails.rejected, (state, action) => {
-//         state.error =
-//           typeof action.payload === 'string'
-//             ? action.payload
-//             : 'An unknown error occurred while updating user details.';
-//       });
-//   },
-// });
+      // Handle signOutUser Thunk
+      .addCase(signOutUser.fulfilled, (state) => {
+        state.authenticated = false;
+        state.currentUser = null;
+        state.loading = false;
+        state.error = null;
+      })
+      .addCase(signOutUser.rejected, (state, action) => {
+        state.error =
+          typeof action.payload === 'string'
+            ? action.payload
+            : 'An unknown error occurred during sign out.';
+      })
 
-// // Export actions
-// export const {
-//   signOut,
-//   updateUser,
-//   setUser,
-//   authenticateUser,
-// } = authSlice.actions;
+      // Handle updateUserDetails Thunk
+      .addCase(updateUserDetails.fulfilled, (state, action: PayloadAction<User>) => {
+        state.currentUser = action.payload;
+        state.loading = false;
+        state.error = null;
+      })
+      .addCase(updateUserDetails.rejected, (state, action) => {
+        state.error =
+          typeof action.payload === 'string'
+            ? action.payload
+            : 'An unknown error occurred while updating user details.';
+      });
+  },
+});
 
-// // Export reducer
-// export default authSlice.reducer;
+// Export actions
+export const {
+  signOut,
+  updateUser,
+  setUser,
+  authenticateUser,
+} = authSlice.actions;
 
-// // Export selector
-// export const selectCurrentUser = (state: RootState) => state.auth.currentUser;
+// Export reducer
+export default authSlice.reducer;
+
+// Export selector
+export const selectCurrentUser = (state: RootState) => state.auth.currentUser;
