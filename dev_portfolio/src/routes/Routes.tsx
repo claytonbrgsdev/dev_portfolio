53338723901a177aaa8de4ccb18c0aa19e55b13b
claytonbrgs <142549801+claytonbrgs@users.noreply.github.com>
2024-11-08 09:05:41 -0300

    routes added

--- dev_portfolio/src/routes/Routes.tsx
+++ dev_portfolio/src/routes/Routes.tsx
@@ -1,77 +1,44 @@
-// import { createBrowserRouter } from 'react-router-dom';
-// import MainLayout from '@components/layout/mainLayout/MainLayout';
-// import About from '@pages/about/About';
-// import Error from '@pages/error/Error';
-// import Register from '@pages/auth/register/Register';
-// import Login from '@pages/auth/login/Login';
-// import Cart from '@pages/shop/cart/Cart';
-// import SuccessPage from '@pages/shop/checkout/sucessPage/SuccessPage';
-// import CancelPage from '@pages/shop/checkout/cancelPage/CancelPage';
-// import UserProfile from '@pages/userProfile/myProfile/UserProfile';
-// import UserProfileEdit from '@pages/userProfile/editMyProfile/UserProfileEdit';
-// import AdminActions from '@pages/admin/adminActions/AdminActions';
-// import ViewProducts from '@pages/admin/manageProducts/ViewProducts/ViewProducts';
-// import AddProduct from '@pages/admin/manageProducts/AddProduct/AddProduct';
-// import MyOrders from '@pages/shop/myOrders/MyOrders';
-// import SingleOrderPage from '@pages/shop/singleOrderPage/SingleOrderPage';
-// import AdminRoute from './AdminRoute';
-// import Shop from '@pages/shop/shop/Shop';
-// import Home from '@pages/home/Home';
-// import Radioshows from '@pages/radioshows/radioshows/Radioshows';
-// import HostRadioshows from '@pages/radioshows/hostRadioshows/HostRadioshows';
-// import MediaPage from '@pages/media/MediaPage';
-// import Catalog from '@pages/catalog/catalog/Catalog';
-// import Artists from '@pages/artists/artists/Artists';
-// import ProtectedRoute from './ProtectedRoute';
-// import AddArtist from '@pages/admin/manageArtist/AddArtist/AddArtist';
-// import AddRelease from '@pages/admin/manageRelease/AddRelease/AddRelease';
-// import AddMedia from '@pages/admin/manageMedia/AddMedia/AddMedia';
-// import AddRadioshow from '@pages/admin/manageRadioshow/AddRadioshow/AddRadioshow';
-// import ViewArtists from '@pages/admin/manageArtist/ViewArtists/ViewArtists';
-// import ViewReleases from '@pages/admin/manageRelease/ViewReleases/ViewReleases';
-// import SingleArtistPage from '@pages/artists/singleArtistPage/SingleArtistPage';
-// import SingleReleasePage from '@pages/catalog/singleReleasePage/SingleReleasePage';
-// import ViewMedia from '@pages/admin/manageMedia/viewMedia/ViewMedia';
-// import ViewRadioshows from '@pages/admin/manageRadioshow/viewRadioshows/ViewRadioshows';
-// import SingleProductPage from '@pages/shop/singleProductPage/SingleProductPage';
-// // import HomeEnvironment from '@components/three/home/homeEnv/HomeEnv';
-// import GetHelp from '@pages/getHelp/GetHelp';
-// import ThankYou from '@pages/thankYou/ThankYou';
-// import Home3DScene from '@pages/home/Home3DScene/components/Home3DScene';
+import { createBrowserRouter } from 'react-router-dom';
+import MainLayout from '@components/layout/mainLayout/MainLayout';
+import About from '@pages/about/About';
+import Error from '@pages/error/Error';
+import Register from '@pages/auth/register/Register';
+import Login from '@pages/auth/login/Login';
+import Home3DScene from '@pages/home/Home3DScene/components/Home3DScene';
 
-// export const router = createBrowserRouter([
-//     {
-//         path: '/',
-//         element: <MainLayout />,
-//         errorElement: <Error />,
-//         children: [
-//             {
-//                 index: true,
-//                 element: <Home />,
-//             },
-//             {
-//                 path: 'home',
-//                 element: <Home />,
-//             },
-//             {
-//                 path: 'about',
-//                 element: <About />,
-//             },
-//             {
-//                 path: 'test-area',
-//                 element: <Home3DScene />,
-//             },
-//         ],
-//     },
-//     {
-//         path: '/register',
-//         element: <Register />,
-//         errorElement: <Error />,
-//     },
-//     {
-//         path: '/login',
-//         element: <Login />,
-//         errorElement: <Error />,
-//     },
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout />,
+        errorElement: <Error />,
+        children: [
+            {
+                index: true,
+                element: <Home />,
+            },
+            {
+                path: 'home',
+                element: <Home />,
+            },
+            {
+                path: 'about',
+                element: <About />,
+            },
+            {
+                path: 'test-area',
+                element: <Home3DScene />,
+            },
+        ],
+    },
+    {
+        path: '/register',
+        element: <Register />,
+        errorElement: <Error />,
+    },
+    {
+        path: '/login',
+        element: <Login />,
+        errorElement: <Error />,
+    },
 
-// ]);
+]);
