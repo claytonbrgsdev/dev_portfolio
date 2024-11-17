786bef563a0517a70ac32a00d4338139157d2d1a
claytonbrgs <142549801+claytonbrgs@users.noreply.github.com>
2024-11-17 06:40:12 -0300

    navbar update

--- dev_portfolio/src/components/layout/navBar/NavBar.tsx
+++ dev_portfolio/src/components/layout/navBar/NavBar.tsx
@@ -1,13 +1,15 @@
 import styles from './NavBar.module.css'
+// import logo1 from './../../../assets/Logos_ClaytonBorgesDev/Asset 4@2x.png'
+// import logo2 from './../../../assets/Logos_ClaytonBorgesDev/Asset 5@2x.png'
+import logo3 from './../../../assets/logos/Asset 6@2x.png'
+
 export default function NavBar() {
     return (
-        <div className={styles['navbarContainer']} >
-            <h1 className={styles['title']} style={{ display: 'flex', margin: "2.5rem 5rem" }}>
-            C B
-            </h1>
-                <div className={styles['menuTab']} >
-                    <a href='contact' className={styles['menuItem']}>contact</a>
-                </div>
+        <div className={styles['navbarContainer']}  style={{ position: "static", top: 0, left: 0 }}>
+            <img src={logo3} style={{ display: 'flex', color: "white", margin: "auto auto auto 2.5rem", filter: "invert()", maxHeight: "2rem", height: "100%", width: "auto" }}/>
+                {/* <div className={styles['menuTab']} >
+                    <a href='contact' style={{ margin: "auto"}} >contact</a>
+                </div> */}
         </div>
     )
 }
